ec03dee8ae55067514018dabd6815e458c026e99
karaner1337 <prove1337@wp.pl>
2020-09-14 22:51:52 +0200

    zadanie 9

---
+++ src/com/company/creatures/EdBile.java
@@ -0,0 +1,5 @@
+package com.company.creatures;
+
+public interface EdBile {
+    void beEaten();
+}
---
+++ src/com/company/creatures/FarmAnimal.java
@@ -0,0 +1,27 @@
+package com.company.creatures;
+
+public class FarmAnimal extends animal implements EdBile {
+
+    String species = "dog";
+    private Double weight;
+
+
+
+    public FarmAnimal(String species, Double weight) {
+        super();
+    }
+
+
+
+    @Override
+
+    public void beEaten() {
+
+    }
+
+    @Override
+
+    public void feed(Double foodWeight) {
+
+    }
+}
